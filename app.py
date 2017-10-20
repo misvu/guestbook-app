902abe0751f1f57df27aa79cca26adf0bdb508da
Anna Vu <Anna Vu>
2017-10-20 19:14:45 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+﻿from flask import Flask, Response, render_template, request
+
+app = Flask(__name__, static_folder="static", static_path="")
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+	title="STOCKHOLM"
+	return render_template('index.html', title=title)
+
+@app.route('/about/')
+def about():
+	return render_template('about.html')
+
+if __name__=='__main__':
+	app.run(port=5000, debug=True)
+
